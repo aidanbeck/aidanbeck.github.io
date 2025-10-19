ac1dd7322aff8ea3f0dbd85a3e55acb19e2be6c5
aidanbeck <aidanmbeck@outlook.com>
2025-10-18 19:22:29 -0500

    3d cube

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css';
+import './styles/3d.css'; // 3d
 import Card from './components/Card';
 
 function App() {
 
   return (
     <>
+
+    <div className="scene">
+      <div className="🧊">
+        <div className="🧊face N">N</div>
+        <div className="🧊face S">S</div>
+        <div className="🧊face E">E</div>
+        <div className="🧊face W">W</div>
+        <div className="🧊face U">U</div>
+        <div className="🧊face D">D</div>
+      </div>
+    </div>
+
      <h1>My Portfolio Projects</h1>
      <section>
       <Card>
