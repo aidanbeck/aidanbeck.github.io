53a2abd1950bab2ee9bc28e451e0d56eae6e4fb2
aidanbeck <aidanmbeck@outlook.com>
2025-10-20 16:53:40 -0500

    MVP structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
 import './App.css';
+import './styles/portfolio.css';
 
 function App() {
 
   return (
     <>
-    
+      <br/>
+      <section>
+        <img className="face" src="face.png"></img>
+        <div>
+          <h1>My name is Aidan, welcome to my site!</h1>
+          <p><strong>About Me</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam nesciunt minus maiores velit, labore est ipsum laborum dignissimos accusamus error dolores ipsa nostrum sapiente impedit doloribus rem totam a numquam!</p>
+          <p><strong>Technical Skills</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam nesciunt minus maiores velit, labore est ipsum laborum dignissimos accusamus error dolores ipsa nostrum sapiente impedit doloribus rem totam a numquam!</p>
+          <p><strong>My Resume</strong></p>
+          <p><strong>Contact Me</strong></p>
+        </div>
+      </section>
+
+      <hr/>
+
+      <section>
+        <img className="face" src="moon.jpg"></img>
+        <div>
+          <h1>Old Cove</h1>
+          <p><strong>About The Project</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam nesciunt minus maiores velit, labore est ipsum laborum dignissimos accusamus error dolores ipsa nostrum sapiente impedit doloribus rem totam a numquam!</p>
+          <p><strong>The Tech Stack</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam nesciunt minus maiores velit, labore est ipsum laborum dignissimos accusamus error dolores ipsa nostrum sapiente impedit doloribus rem totam a numquam!</p>
+          <p><strong>Deployment</strong></p>
+          <p><strong>GitHub</strong></p>
+        </div>
+      </section>
+
+      <section>
+        <img className="face" src="moon.jpg"></img>
+        <div>
+          <h1>Syntaxlor</h1>
+          <p><strong>About The Project</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam nesciunt minus maiores velit, labore est ipsum laborum dignissimos accusamus error dolores ipsa nostrum sapiente impedit doloribus rem totam a numquam!</p>
+          <p><strong>The Tech Stack</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam nesciunt minus maiores velit, labore est ipsum laborum dignissimos accusamus error dolores ipsa nostrum sapiente impedit doloribus rem totam a numquam!</p>
+          <p><strong>GitHub</strong></p>
+        </div>
+      </section>
+
+      <hr/>
+
     </>
   )
 }
