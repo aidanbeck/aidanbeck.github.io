784a3f70d002d14c47f250d8ea882f4aeb3d49f8
aidanbeck <aidanmbeck@outlook.com>
2025-10-20 14:41:15 -0500

    separated 3d scene into it's own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import './App.css';
-import './styles/3d.css'; // 3d
-import Card from './components/Card';
 
 function App() {
 
   return (
     <>
-    <div className="scene">
-      <div className="🧊">
-        <div className="🧊face N">N</div>
-        <div className="🧊face S">S</div>
-        <div className="🧊face E">E</div>
-        <div className="🧊face W">W</div>
-        <div className="🧊face U">U</div>
-        <div className="🧊face D">V</div>
-        <div className="🧊origin"></div>
-      </div>
-    </div>
+    
     </>
   )
 }
---
+++ src/components/Scene.jsx
@@ -0,0 +1,17 @@
+import '../styles/3d.css';
+
+function Scene() {
+    return (
+        <div className="scene">
+            <div className="🧊">
+                <div className="🧊face N">N</div>
+                <div className="🧊face S">S</div>
+                <div className="🧊face E">E</div>
+                <div className="🧊face W">W</div>
+                <div className="🧊face U">u</div>
+                <div className="🧊face D">d</div>
+                <div className="🧊origin"></div>
+            </div>
+        </div>
+    );
+}
